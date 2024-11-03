bac7404c1ab3fb5bfb655d2581539e86c2cd360d
DuarHBD <152714636+DuarHBD@users.noreply.github.com>
2024-11-03 08:03:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ const moneyLi=document.getElementById('list-money')
 const calbtn=document.getElementById('calbtn')
 const tablecal=document.getElementById("display-cal")
 const cal_box=document.getElementById('cal-box')
+input_name.addEventListener('focus', function() {
+    // เปลี่ยน viewport ชั่วคราว
+    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
+});
+
+input_name.addEventListener('blur', function() {
+    // คืนค่า viewport กลับ
+    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1");
+});
+input_money.addEventListener('focus', function() {
+    // เปลี่ยน viewport ชั่วคราว
+    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
+});
+
+input_money.addEventListener('blur', function() {
+    // คืนค่า viewport กลับ
+    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1");
+});
 
 
 const Prename=[
@@ -248,22 +266,5 @@ btnPutmoney.addEventListener('click',addMoneybyuser)
 calbtn.addEventListener('click',calculate)
 hideBothDivs()
 init()
-input_name.addEventListener('focus', function() {
-    // เปลี่ยน viewport ชั่วคราว
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
-});
 
-input_name.addEventListener('blur', function() {
-    // คืนค่า viewport กลับ
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1");
-});
-input_money.addEventListener('focus', function() {
-    // เปลี่ยน viewport ชั่วคราว
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
-});
-
-input_money.addEventListener('blur', function() {
-    // คืนค่า viewport กลับ
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=1");
-});
 
